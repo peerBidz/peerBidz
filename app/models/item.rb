6adc5797849aae102455f49401579b111b2526ec
Chris Dorros <chris.dorros@sv.cmu.edu>
2011-10-10 16:37:44 -0400

    Added small photo to items listing. Added restriction on image attachments. CD, WK

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,8 @@
 class Item < ActiveRecord::Base
 
-  has_attached_file :photo
+  has_attached_file :photo, :styles => { :small => "150x150>" }
+
+  validates_attachment_presence :photo
+  validates_attachment_size :photo, :less_than => 5.megabytes
+  validates_attachment_content_type :photo, :content_type => ['image/jpeg', 'images/png']
 end
