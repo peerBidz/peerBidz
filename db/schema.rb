405122d4e86303e6a0263506439751756ad02717
Chris Dorros <chris.dorros@sv.cmu.edu>
2011-10-14 20:08:24 -0400

    Added subcategories to item listing. Added image sizes. Added categories table/models/view/controller. -CD/WK

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111008205911) do
+ActiveRecord::Schema.define(:version => 20111014211848) do
+
+  create_table "categories", :force => true do |t|
+    t.string   "name"
+    t.integer  "parentID"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "items", :force => true do |t|
     t.string   "title"
