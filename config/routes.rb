f17582da7960bab3b2f00c701aa01b9a75988942
amalyala <lenin@cmu.edu>
2011-10-14 22:22:50 -0400

    added active_admin partially LM AD

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,22 @@
 BestBay::Application.routes.draw do
 
+  #ActiveAdmin.routes(self)
 
-  devise_for :admins
+  #devise_for :admin_users, ActiveAdmin::Devise.config
 
   devise_for :users
 
   resources :items
   resources :users
 
-  match '/allusers',  :to => 'users#index'
   match '/contact', :to => 'pages#contact'
   match '/items',   :to => 'pages#items'
   match '/help',    :to => 'pages#help'
   match '/about',   :to => 'pages#about'
 
-  match '/welcome',   :to => 'pages#adminHome'
-
   root :to => 'pages#home'
 
-  match '/admin' => "welcome#index", :as => :admin_root
+
 
 
 
