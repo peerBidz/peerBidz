985f1b38dcc86826fd60cc0b1868a612bcac5995
amalyala <lenin@cmu.edu>
2011-10-16 00:58:06 -0400

    Bidding is working, but item posting unsuccessful LM AD

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,9 @@
 class Item < ActiveRecord::Base
+  #attr_accessor_with_default :bidvalue, 0
+  #attr_accessor_with_default :starting_price, 0
+
+
+
 
   has_attached_file :photo, :styles => { :small => "150x150", :medium => "450x450>", :large => "650x650>" }
 
@@ -6,8 +11,10 @@ class Item < ActiveRecord::Base
   validates_attachment_size :photo, :less_than => 5.megabytes
   validates_attachment_content_type :photo, :content_type => ['image/jpeg', 'images/png']
 
-  #validates_numericality_of :bidvalue
-  #validates_numericality_of :bidvalue, :greater_than => :bidvalue_was
+
+  #validates_numericality_of :bidvalue,
+  validates_numericality_of :starting_price
+  validates_numericality_of :bidvalue, :greater_than => :bidvalue_was
 
   belongs_to :category
 end
