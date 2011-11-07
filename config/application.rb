144f5aa1836bc5064d11c2aa3f04ab3a41efcab1
amalyala <lenin@cmu.edu>
2011-11-07 14:46:19 -0500

    rufus scheduler working for winner_notify LM

--- config/application.rb
+++ config/application.rb
@@ -42,5 +42,9 @@ module BestBay
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
+    config.time_zone = 'Eastern Time (US & Canada)'
+    config.active_record.default_timezone = 'Eastern Time (US & Canada)'
+    config.gem "rufus-scheduler", :lib => "rufus/scheduler"
+
   end
 end
