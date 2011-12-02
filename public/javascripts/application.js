560a715181530df761a74a1add04e30d3808c4fc
Chris Dorros <chris.dorros@sv.cmu.edu>
2011-12-02 00:18:59 -0500

    fixed issue where item price not correct in search result view -CD

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -3,8 +3,22 @@ $(function() {
 		setTimeout(updateNotifications, 1000);
 	}
 });
+/*
+$(function() {
+	//if($("#current-price").length > 0) {
+		setTimeout(updateItemPrice, 1000);
+	//}
+});
+*/
 function updateNotifications() {
 	var user_id = $(".notifications").attr("data-user-id")
 	$.getScript("/notifications.js?user_id=" + user_id)
 	setTimeout(updateNotifications, 10000);
 }
+/*
+function updateItemPrice() {
+	alert("updateItemPrice")
+	//$.getScript("/items.js")
+	setTimeout(updateItemPrice, 10000);
+}
+*/
