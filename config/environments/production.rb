59a850b66be0f31b19f742644a60aa92763580fe
amalyala <lenin@cmu.edu>
2011-12-02 19:12:31 -0500

    added notification type and watch list

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,4 +51,24 @@ config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+
+  config.action_mailer.default :charset => "utf-8"
+
+  config.action_mailer.default_url_options = { :host => 'best-bay' }
+  config.active_support.deprecation = :log
+
+  config.action_mailer.smtp_settings ={
+    :enable_starttls_auto => true,
+    :address            => 'smtp.gmail.com',
+    :port               => 587,
+    :tls                => true,
+    :domain             => 'gmail.com',
+    :authentication     => :plain,
+    :user_name          => "rockingonrails",
+    :password           => 'cmuinims23'
+  }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111126234645) do
+ActiveRecord::Schema.define(:version => 20111202232726) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.integer  "resource_id",   :null => false
@@ -86,6 +86,7 @@ ActiveRecord::Schema.define(:version => 20111126234645) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "message"
+    t.string   "notification_type"
   end
 
   create_table "searches", :force => true do |t|
