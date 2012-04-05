3548858da1b07dedc755f03e139577cbfe726f4f
Jenna K <jkallahe@andrew.cmu.edu>
2012-04-04 21:24:10 -0400

    fix rpc

--- bootstrap_server/app/controllers/rpc_controller.rb
+++ bootstrap_server/app/controllers/rpc_controller.rb
@@ -6,12 +6,13 @@ class RpcController < ApplicationController
   exposes_xmlrpc_methods
 
   add_method 'Container.is_seller' do |emailaddress|
-
-     @myvar = User.where("email = :et", {:et => emailaddress}) 
-     if @myvar.is_seller?
+     emailaddress.sub("%40", "@");
+     @myvar = User.where("email = :et", {:et => emailaddress}).first 
+     if @myvar.is_seller == 1
        { "value" => "1" }
      else
        { "value" => "0" }
+     end
   end
 
 end
