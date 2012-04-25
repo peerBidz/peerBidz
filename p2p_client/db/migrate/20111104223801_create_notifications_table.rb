8387e7be946c3076f7ddfbe4d06eaf7595cefa52
Naveen Chekuri <nchekuri@andrew.cmu.edu>
2012-04-25 17:54:34 -0400

    Notifications

--- p2p_client/db/migrate/20111104223801_create_notifications_table.rb
+++ p2p_client/db/migrate/20111104223801_create_notifications_table.rb
@@ -1,7 +1,6 @@
 class CreateNotificationsTable < ActiveRecord::Migration
   def self.up
 	create_table :notifications do |t|
-	  t.integer :user_id
 	  t.string :type
 	  t.integer :item_id
 	  t.boolean :delivered
