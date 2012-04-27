c2c209b348c4d1918181a5b603a38149cc048790
Prajakta Karandikar <prajakta.karandikar@gmail.com>
2012-04-26 22:19:01 -0400

    Buyer browse more stuff

--- p2p_client/app/controllers/items_controller.rb
+++ p2p_client/app/controllers/items_controller.rb
@@ -300,7 +300,7 @@ class ItemsController < ApplicationController
   		end
 		@is_parentpresent = Ipaddress.where("category = :ct AND iptype = :it", {:ct => params[:browse], :it => "parent"})
 		if @is_parentpresent != nil
-			if @is_parentpresent.count == 0
+			if @is_parentpresent.count != 0
 				@is_parentbackuppresent = Ipaddress.where("category = :ct AND iptype = :it", {:ct => params[:browse], :it => "parentbackup"})
 				puts "Entered here"
 				if @is_parentbackuppresent != nil
