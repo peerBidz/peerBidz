a9c15d337d3e6ff83054a270dc06460516bd4adf
Jenna K <jkallahe@andrew.cmu.edu>
2012-04-26 20:53:25 -0400

    completing code for payment of items

--- p2p_client/db/schema.rb
+++ p2p_client/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120426223756) do
+ActiveRecord::Schema.define(:version => 20120426231559) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.integer  "resource_id",   :null => false
@@ -52,6 +52,13 @@ ActiveRecord::Schema.define(:version => 20120426223756) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "item_id"
+  end
+
+  create_table "bitcoins", :force => true do |t|
+    t.integer  "itemid"
+    t.string   "address"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "carts", :force => true do |t|
